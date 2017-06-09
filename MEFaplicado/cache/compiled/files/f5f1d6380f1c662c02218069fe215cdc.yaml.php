e60252704d5d038a3f84ee75788409b67fff3776
markinho <markinho@madPC-i7>
2017-06-09 16:57:11 -0300

    MEFaplicado

--- MEFaplicado/cache/compiled/files/f5f1d6380f1c662c02218069fe215cdc.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/home/markinho/Dropbox/argenta-web.github.io/MEFaplicado/user/localhost/config/security.yaml',
-    'modified' => 1496877832,
-    'data' => [
-        'salt' => 'L34mUlCTvGoVBf'
-    ]
-];
