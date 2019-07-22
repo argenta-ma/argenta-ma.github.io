fd7ea7eabef8eec09edc9df477589d4530e6e4f8
Marco André Argenta <marco.argenta@gmail.com>
2019-07-22 18:46:39 -0300

    Site teste

---
+++ site_teste/js/mathjaxhelper.js
@@ -0,0 +1,7 @@
+MathJax.Hub.Config({
+  "tex2jax": { inlineMath: [ [ '$', '$' ] ] },
+  config: ["MMLorHTML.js"],
+  jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+  extensions: ["MathMenu.js", "MathZoom.js"],
+  menuSettings: { zoom: "Click" }
+});
